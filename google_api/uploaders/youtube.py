194ad6be4b0d82f3728100696948235c1261b39f
LUS1N <sinkus.lukas@gmail.com>
2018-01-20 19:17:52 +0100

    clean up

--- google_api/uploaders/youtube.py
+++ google_api/uploaders/youtube.py
@@ -1,15 +1,17 @@
 from google_api.uploaders.base import BaseUploader
-from apiclient.errors import HttpError
-from apiclient.http import MediaFileUpload
-
 
 class Youtube(BaseUploader):
+    """Class handling uploading videos yo Youtube
+    """
 
     def __init__(self, appName):
         BaseUploader.__init__(self, appName, "youtube")
 
-    def upload_video(self, path, name, description=None, tags=None, category=None, privacy_status='public', max_retries=10):
-
+    def upload_video(self, path, name, description=None, tags=None, category=None, privacy_status='public'):
+        """ Uploads a video file to youtube
+        Returns:
+            Response from the api
+        """
         body = {
             'snippet': {
                 'title': name,
