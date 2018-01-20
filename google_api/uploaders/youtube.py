cb877b5a6e2295855d24567fad25740939bc98ca
LUS1N <sinkus.lukas@gmail.com>
2018-01-20 19:15:14 +0100

    youtube uploader

--- google_api/uploaders/youtube.py
+++ google_api/uploaders/youtube.py
@@ -1,10 +1,26 @@
-from google_api.uploaders.base import BaseUploader 
+from google_api.uploaders.base import BaseUploader
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+
 
 class Youtube(BaseUploader):
 
     def __init__(self, appName):
         BaseUploader.__init__(self, appName, "youtube")
-        print(self.credentials.value)
-    
 
-    
+    def upload_video(self, path, name, description=None, tags=None, category=None, privacy_status='public', max_retries=10):
+
+        body = {
+            'snippet': {
+                'title': name,
+                'description': description,
+                'tags': tags,
+                'category':  category
+            },
+            'status': {
+                'privacyStatus': privacy_status
+            }
+        }
+
+        return self.service.videos().insert(part=",".join(
+            body.keys()), body=body, media_body=path).execute()
