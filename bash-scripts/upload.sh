fc550155741fc2821ede82493d26dc60362a336a
LUS1N <sinkus.lukas@gmail.com>
2018-01-21 21:37:02 +0100

    remove dangling qupote

--- bash-scripts/upload.sh
+++ bash-scripts/upload.sh
@@ -2,4 +2,4 @@
 echo "Starting google upload"
 python google_upload.py pilapse "$OUT_DIRECTORY/$current_date.h264" "$current_date" "Timelapse in Risskov, Aarhus on $current_date" --tags Timelapse, Denmark, Aarhus, Construction
 
-echo Drive and Youtube upload complete.".
+echo Drive and Youtube upload complete.
