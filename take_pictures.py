2ce68c3645419a68d4a3ed0086c06e3a645b4894
LUS1N <sinkus.lukas@gmail.com>
2018-01-21 15:16:14 +0100

    script for taking pictures and storing them

---
+++ take_pictures.py
@@ -0,0 +1,75 @@
+import argparse
+from datetime import datetime
+# from picamera import PiCamera
+from time import sleep
+import os
+
+
+def take_pictures(film_until, dir, picture_number, resolution, interval, annotate=True):
+    """ Takes a picture every *interval* seconds.
+        Saves it in *dir*, named in a format %06d.png.
+        Starting with parameter *picture_number*
+        Films until *film_until* hour (integer).
+        Resolution is set to *resolution* (tuple of integers).
+        Annotates each picture with HH:MM on top of the picture if
+        *annotate* == True
+    """
+    camera = PiCamera()
+    camera.resolution = resolution
+    camera.start_preview()
+
+    if not picture_number:
+        picture_number = 1
+
+    if not resolution:
+        resolution = (1920, 1080)
+
+    if not interval:
+        interval = 4
+
+    while True:
+        sleep(interval)
+        time = datetime.now().time()
+
+        if annotate:
+            camera.annotate_text = "%02d : %02d" % (time.hour, time.minute)
+
+        camera.capture('%s/%06d.png' % (dir, picture_number))
+
+        print("%d - %02d : %02d : %02d" %
+              (picture_number, time.hour, time.minute, time.second))
+
+        picture_number += 1
+
+        if time.hour == film_until:
+            break
+
+    camera.stop_preview()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'film_until', help='Stop taking pictures at this hour.', type=int)
+
+    parser.add_argument(
+        'dir', help='Put pictures in this directory.', type=str)
+
+    parser.add_argument('-p', '--picture_number',
+                        help='Start naming pictures at this number.', type=int)
+
+    parser.add_argument(
+        '-i', '--interval', help='Seconds between pictures', type=int)
+
+    parser.add_argument("-a", "--annotate", action="store_true",
+                        help="Annotate pictures with time it was taken")
+
+    arguments = parser.parse_args()
+    print(arguments)
+
+    take_pictures(film_until=arguments.film_until,
+                  dir=arguments.dir,
+                  picture_number=arguments.picture_number,
+                  resolution=arguments.resolution,
+                  interval=arguments.interval,
+                  annotate=arguments.annotate)
