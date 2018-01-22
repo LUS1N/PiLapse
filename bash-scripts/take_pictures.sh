7bb021c60190528485c81b7aa2c59e4b7848af7e
Lukas Sinkus <lsi@novicell.dk>
2018-01-22 19:39:00 +0100

    annotate

--- bash-scripts/take_pictures.sh
+++ bash-scripts/take_pictures.sh
@@ -2,5 +2,5 @@ echo "taking pics start at $stills_start"
 
 stills_start="$1"
 echo "Starting taking pictures"
-python ./take_pictures.py $TO_TIME $STILLS_DIRECTORY -p "$stills_start"
+python ./take_pictures.py $TO_TIME $STILLS_DIRECTORY -p "$stills_start" -a
 echo "Taking pictures complete"
